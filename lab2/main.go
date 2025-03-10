cc763521f2ebc90dbf29335bd8cb9e2f62f6b9c5
Piotr Gruchała <pgruchala59@gmail.com>
2025-03-10 20:43:11 +0100

    lab2

--- lab2/main.go
+++ lab2/main.go
@@ -29,6 +29,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 	var items []Entry
 	for _,record := range records[1:] {
 		item := Entry{
@@ -42,18 +43,36 @@ func main() {
 		}
 		items = append(items, item)
 	}
+	fmt.Println("Sort by Code")
 	sort.Slice(items, func(i, j int) bool {
 		return items[i].Code < items[j].Code
 	})
 	for _,item := range items {
 		fmt.Println(item)
 	}
-
+	fmt.Println("-------------------------------------------------")
+	fmt.Println("Sort by ENLabel")
 	sort.Slice(items, func(i, j int) bool {
 		return items[i].ENLabel < items[j].ENLabel
 	})
 	for _,item := range items {
 		fmt.Println(item)
 	}
+
+	fmt.Println("-------------------------------------------------")
+	fmt.Println("Średnia długość nazwy dla każdego języka")
+	var sumDE,sumEN,sumES,sumFR,sumPT int
+	for _,item := range items {
+		sumDE += len(item.DELabel)
+		sumEN += len(item.ENLabel)
+		sumES += len(item.ESLabel)
+		sumFR += len(item.FRLabel)
+		sumPT += len(item.PTLabel)
+	}
+	fmt.Println("DE: ",float64(sumDE)/float64(len(items)))
+	fmt.Println("EN: ",float64(sumEN)/float64(len(items)))
+	fmt.Println("ES: ",float64(sumES)/float64(len(items)))
+	fmt.Println("FR: ",float64(sumFR)/float64(len(items)))
+	fmt.Println("PT: ",float64(sumPT)/float64(len(items)))
 
 }
